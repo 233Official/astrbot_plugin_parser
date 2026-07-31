4238e938b4e4641e60112ff109e73875bb9124db
aurelee <1425153862@qq.com>
2026-07-31 14:33:07 +0800

    fix(bili): add bilibili mp4 video stream download url

--- core/sender.py
+++ core/sender.py
@@ -28,8 +28,8 @@ from .data import (
 )
 from .exception import (
     DownloadException,
-    DurationLimitException,
     DownloadLimitException,
+    DurationLimitException,
     SizeLimitException,
     ZeroSizeException,
 )
